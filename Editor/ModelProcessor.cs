1cafa602188e56de2ed6345cbf2dee899035468a
darkon76 <adrianr76@gmail.com>
2016-04-20 11:13:10 -0500

    Added files via upload

---
+++ Editor/ModelProcessor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+
+public class ModelProcessor : AssetPostprocessor
+{
+    private void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = (ModelImporter)assetImporter;
+        if (modelImporter != null)
+        {
+            if (assetPath.Contains("Models"))
+            {
+                modelImporter.addCollider = false;
+                modelImporter.importBlendShapes = false;
+                modelImporter.optimizeMesh = true;
+                modelImporter.meshCompression = ModelImporterMeshCompression.High;
+
+                #region Static
+
+                if (assetPath.Contains("Static"))
+                {
+                    modelImporter.generateSecondaryUV = true;
+                    modelImporter.importAnimation = false;
+                    modelImporter.animationType = ModelImporterAnimationType.None;
+                }
+                #endregion Static
+
+                #region LowPoly
+
+                if (assetPath.Contains("LowPoly"))
+                {
+                    modelImporter.importNormals = ModelImporterNormals.Calculate;
+                    modelImporter.normalSmoothingAngle = 0;
+                }
+                #endregion LowPoly
+            }
+        }
+    }
+}
---
+++ Editor/SpritePostProcessor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpritePostProcessor : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        if (textureImporter == null)
+        {
+            return;
+        }
+        if (assetPath.Contains("Sprite"))
+        {
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.spritePixelsPerUnit = 16;
+            textureImporter.mipmapEnabled = false;
+            textureImporter.filterMode = FilterMode.Point;
+            textureImporter.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+
+            #region SpritePacker
+
+            string path = assetPath;
+            path = path.Replace("Assets/Sprites/", "");
+            string[] pathSplit = path.Split('/');
+            if (pathSplit.Length > 1)
+            {
+                textureImporter.spritePackingTag = pathSplit[0];
+            }
+            #endregion SpritePacker
+        }
+    }
+
+    #region PostProcess
+
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        if (textureImporter == null)
+        {
+            return;
+        }
+        string[] path = assetPath.Split(new char[] { '/', '\\' });
+        string fullFileName = path[path.Length - 1];
+        string[] ext = fullFileName.Split(new char[] { '.' });
+        string fileName = ext[0];
+        if (fileName.Contains("_CR"))
+        {
+            string[] split = fileName.Split(new string[] { "_CR" }, StringSplitOptions.None);
+            fileName = split[0];
+            string[] code = split[1].Split('x');
+            int width = texture.width / int.Parse(code[0]);
+            int height = texture.height / int.Parse(code[1]);
+
+            List<SpriteMetaData> newData = new List<SpriteMetaData>();
+            for (int i = 0; i < texture.width; i += width)
+            {
+                for (int j = texture.height; j > 0; j -= height)
+                {
+                    SpriteMetaData smd = new SpriteMetaData();
+                    smd.pivot = new Vector2(0.5f, 0.5f);
+                    smd.alignment = 9;
+                    smd.name = fileName + " " + (texture.height - j) / height + "_" + i / width;
+                    smd.rect = new Rect(i, j - height, width, height);
+
+                    newData.Add(smd);
+                }
+            }
+
+            textureImporter.spriteImportMode = SpriteImportMode.Multiple;
+            textureImporter.spritesheet = newData.ToArray();
+        }
+    }
+
+    #endregion PostProcess
+}
